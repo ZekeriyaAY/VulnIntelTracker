d576129b08dc278c47d942a87d21ff9c4e78aca6
Zekeriya AY <zekeriya@zekeriyaay.com>
2023-07-29 00:42:18 +0300

    Main Files Updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
-from .myapp import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+login = LoginManager(app)
+login.login_view = 'login'  # redirect to login page if not logged in
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from .user import models
+from app import routes
